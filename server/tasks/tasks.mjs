0a3ed36d7504643697d78dc9634b2d8d1c6a4bf7
CaptainAlpha04 <aliimran.iam59@gmail.com>
2024-07-18 20:00:52 +0500

    feat: Add Google Generative AI integration for Software Engineer task

--- server/tasks/tasks.mjs
+++ server/tasks/tasks.mjs
@@ -1,6 +1,16 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const genAI = new GoogleGenerativeAI(Math.random() > 0.5? process.env.ACTOR_MODEL_API_ONE: process.env.ACTOR_MODEL_API_TWO);
+
 const tasks = {
     'Software Engineer': async function(instructions) {
-        console.log('Software Engineer task');
+        const model = genAI.getGenerativeModel({
+            model: 'gemini-1.5-flash',
+            systemInstruction: instructions,
+        });
+
+        const res = await model.generateContent();
+        return res.response.text();
     },
     'Graphics Designer': async function(instructions) {
         console.log('Graphics Designer task');
